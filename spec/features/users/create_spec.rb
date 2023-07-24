b1cbb846bc749eefefb972887437e46577f858ea
Blaine Glasgow <glasgowb@outlook.com>
2023-07-23 17:56:47 -0700

    Modify testing for create user with authentication to match user story of challenge

--- spec/features/users/create_spec.rb
+++ spec/features/users/create_spec.rb
@@ -1,20 +1,35 @@
 require "rails_helper"
 
+# As a visitor
+# When I visit `/register`
+# I see a form to fill in my name, email, password, and password confirmation.
+# When I fill in that form with my name, email, and matching passwords,
+# I'm taken to my dashboard page `/users/:id`
+
 RSpec.describe "User Registration Page" do
   describe "Register new User" do
     it "Sees form for a new user" do
-      visit "/"
-      click_button "Create New User"
-      expect(current_path).to eq users_register_index_path
-      expect(page).to have_field("email")
-      expect(page).to have_field("name")
-      expect(page).to have_button("Register")
-      fill_in "email", with: "email@email.com"
-      fill_in "name", with: "Email"
-      click_button "Register"
+      visit "/register"
+      save_and_open_page
+      click_button "Register as a User"
+
+      expect(current_path).to eq(new_user_path)
+      # expect(current_path).to eq users_register_index_path
+
+      name = "President Person"
+      email = "user@email.com"
+      password = "password"
+
+      fill_in :name, with: name
+      fill_in :email, with: email
+      fill_in :password, with: password
+
+      click_on "Create User"
 
       user = User.last
       expect(current_path).to eq user_path(user.id)
     end
   end
+
+
 end
