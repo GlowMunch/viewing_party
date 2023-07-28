5bcf786eecc447945f52795888837eaa49005289
Blaine Glasgow <glasgowb@outlook.com>
2023-07-27 22:35:08 -0700

    Only users can see user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,18 +6,23 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    if current_user
+      @user = User.find(params[:id])
 
-    @viewing_parties = @user.viewing_parties
-    @viewing_users = @user.viewing_users
-    @host = @viewing_users.where(host: "YES")
-    if @user.viewing_parties.count >= 1
-      conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-        faraday.headers["Authorization"] = ENV["api_access_key"]
+      @viewing_parties = @user.viewing_parties
+      @viewing_users = @user.viewing_users
+      @host = @viewing_users.where(host: "YES")
+      if @user.viewing_parties.count >= 1
+        conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+          faraday.headers["Authorization"] = ENV["api_access_key"]
+        end
+        response = conn.get("/3/search/movie/#{@viewing_parties[0].api_movie_id}")
+        data = JSON.parse(response.body, symbolize_names: true)
+        @movie = PopularMovie.new(data)
       end
-      response = conn.get("/3/search/movie/#{@viewing_parties[0].api_movie_id}")
-      data = JSON.parse(response.body, symbolize_names: true)
-      @movie = PopularMovie.new(data)
+    else
+      redirect_to root_path
+      flash[:message] = "Please Login to View Dashboard"
     end
   end
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   def index
     if session[:user_id]
       @user = User.find(session[:user_id])
+      @users = User.all
     end
     unless cookies[:greeting]
       cookies[:greeting] = "HELLO"
